21293c30feceec18e71eb683a425da15a5e9b02b
Simon G <simon.giselsson@gmail.com>
2022-12-05 08:38:02 +0100

    dag5 klar

---
+++ day5/day5-1.py
@@ -0,0 +1,29 @@
+
+
+score = ""
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    i = 1
+    b = {}
+    
+    for j in range(9):
+        b[j+1] = []
+    print(b)
+    for line in lines:
+            if i < 9:
+                for j in range(9):
+                    if line[j*4+1] != " ":
+                        b[j+1].append(line[j*4+1])
+                        
+            elif i > 10:
+                spl = line.split()
+                count = int(spl[1])
+                fromS = int(spl[3])
+                toS = int(spl[5])
+                for j in range(count):
+                    b[toS].insert(0, b[fromS].pop(0))
+            i += 1
+    for j in range(9):
+        score += b[j+1][0]
+
+print(score) 
---
+++ day5/day5-2.py
@@ -0,0 +1,30 @@
+
+
+score = ""
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    i = 1
+    b = {}
+    
+    for j in range(9):
+        b[j+1] = []
+    for line in lines:
+            if i < 9:
+                for j in range(9):
+                    if line[j*4+1] != " ":
+                        b[j+1].append(line[j*4+1])
+                        
+            elif i > 10:
+                spl = line.split()
+                count = int(spl[1])
+                test = count
+                fromS = int(spl[3])
+                toS = int(spl[5])
+                for j in range(count):
+                    b[toS].insert(0, b[fromS].pop(test-1-j))
+            i += 1
+    
+    for j in range(9):
+        score += b[j+1][0]
+
+print(score) 
