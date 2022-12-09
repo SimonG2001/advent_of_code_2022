414cad961e843aa683dbd9f8964efdecf26a9f8d
Simon G <simon.giselsson@gmail.com>
2022-12-09 10:16:41 +0100

    dag9 klar

---
+++ day9/day9-2.py
@@ -0,0 +1,86 @@
+
+def isAdjacent(hx, hy, tx, ty):
+    return abs(hx - tx) <= 1 and abs(hy - ty) <= 1
+
+def moveTail(hx, hy, tx, ty):
+    if not isAdjacent(hx, hy, tx, ty):
+        if hx == tx:
+            if hy > ty:
+                ty += 1
+            else:
+                ty -= 1
+        elif hy == ty:
+            if hx > tx:
+                tx += 1
+            else:
+                tx -= 1
+                
+        elif hx == tx + 2 and hy == ty + 2:
+            tx += 1
+            ty+= 1
+        elif hx == tx - 2 and hy == ty - 2:
+            tx -= 1
+            ty-= 1
+        elif hx == tx + 2 and hy == ty - 2:
+            tx += 1
+            ty -= 1
+        elif hx == tx - 2 and hy == ty + 2:
+            tx -= 1
+            ty+= 1
+
+        else:
+            if tx + 1 == hx or tx - 1 == hx:
+                tx = hx
+                if hy - ty > 0:
+                    ty += 1
+                else:
+                    ty -= 1
+            else:
+                ty = hy
+                if hx - tx > 0:
+                    tx += 1
+                else:
+                    tx -= 1
+        
+    return tx, ty
+
+
+with open('input.txt', 'r') as f:
+    
+    visited = []
+    tails = {}
+    for i in range(10):
+        tails[i] = [0, 0]
+    visited.append("0 0")
+    headV = []
+
+    headV.append("0 0")
+    lines = f.readlines()
+
+    for line in lines:
+        lin = line.split()
+        for i in range(int(lin[1])):
+            #print("head: " + str(hx) + " " + str(hy) + " tail: " + str(tx) + " " + str(ty))
+            if lin[0] == "U":
+                tails[0][1] += 1
+            elif lin[0] == "R":
+                tails[0][0] += 1
+            elif lin[0] == "D":
+                tails[0][1] -= 1
+            elif lin[0] == "L":
+                tails[0][0] -= 1
+            hStr = str(tails[0][0]) + " " + str(tails[0][1])
+            if headV.count(hStr) == 0:
+                headV.append(hStr)
+            
+            for i in range(9):
+                tails[i+1][0], tails[i+1][1] = moveTail(tails[i][0], tails[i][1], tails[i+1][0], tails[i+1][1])
+
+            tailStr = str(tails[9][0]) + " " + str(tails[9][1])
+            if visited.count(tailStr) == 0:
+                visited.append(tailStr)
+            
+
+
+print(len(visited))
+
